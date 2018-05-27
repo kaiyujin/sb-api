aeaf4e7b7a56354f12ca8afaf8f168be9ee584e0
daisuke shiraishi <kaiyujin@gmail.com>
2018-05-28 01:53:44 +0900

    clean import

--- src/main/java/com/kaiyujin/sb/aop/AspectLog.java
+++ src/main/java/com/kaiyujin/sb/aop/AspectLog.java
@@ -2,11 +2,8 @@ package com.kaiyujin.sb.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
