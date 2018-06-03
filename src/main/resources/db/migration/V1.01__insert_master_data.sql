cd528381ffb39af4be96a398b382eb1ef965d0ff
daisuke shiraishi <kaiyujin@gmail.com>
2018-06-04 06:23:39 +0900

    add masters data

--- src/main/resources/db/migration/V1.01__insert_master_data.sql
+++ src/main/resources/db/migration/V1.01__insert_master_data.sql
@@ -3,5 +3,18 @@ insert into users (password, name, email, is_admin, created_by, created_at, upda
   ('test', 'admin', 'test@test.com', true, 1, now(), 1, now())
 ;
 -- 国マスタ
+insert into countries(code, name) values
+  ('JPN', 'Japan'),
+  ('VNM', 'Vietnam')
+;
 -- 言語マスタ
+insert into languages(code, name) values
+  ('en', 'English'),
+  ('ja', 'Japanese'),
+  ('vi', 'Vietnamese')
+;
 -- タイムゾーンマスタ
+insert into timezones(code, name, interval_time) values
+  ('JST','Japan Standard Time', INTERVAL '9 hour'),
+  ('ICT','Indochina Time', INTERVAL '7 hour')
+;
