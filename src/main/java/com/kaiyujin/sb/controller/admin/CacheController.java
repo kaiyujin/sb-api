2b1fd315d2b236a0d20fee62f62e7a0c2475422a
Daisuke Shiraishi <kaiyujin@gmail.com>
2018-07-04 15:34:48 +0900

    add cacheController

--- src/main/java/com/kaiyujin/sb/controller/admin/CacheController.java
+++ src/main/java/com/kaiyujin/sb/controller/admin/CacheController.java
@@ -1,4 +1,18 @@
 package com.kaiyujin.sb.controller.admin;
 
+import com.kaiyujin.sb.common.Constants;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Constants.CLIENT_API_BASE_URL+"/cache")
 public class CacheController {
+
+    @RequestMapping(value = "", method = RequestMethod.DELETE)
+    @CacheEvict(cacheNames = {"countries","user","timezones"}, allEntries = true)
+    public String clear() {
+        return "{ \"status\": \"OK\" }";
+    }
 }
